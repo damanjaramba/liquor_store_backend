11135b17805a71fce69f1f920ea32af1129d4b0f
Morgan Ogola <morganogola@gmail.com>
2025-04-23 12:33:46 +0300

    Refactor LiquorController: update API endpoint structure and remove unused methods

--- src/main/java/com/example/liquorstore/controller/LiquorController.java
+++ src/main/java/com/example/liquorstore/controller/LiquorController.java
@@ -2,6 +2,7 @@ package com.example.liquorstore.controller;
 
 import com.example.liquorstore.dto.LiquorDto;
 import com.example.liquorstore.entity.Liquor;
+import com.example.liquorstore.enums.Category;
 import com.example.liquorstore.service.LiquorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,29 +10,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("public/api/v1")
+@RequestMapping("liquor/api/v1")
 public class LiquorController {
     @Autowired
     public LiquorService liquorService;
 
-    @PostMapping("/addLiquor")
-    public ResponseEntity<?> saveliquor(@RequestBody LiquorDto liquorDto) {
-        try {
-            Liquor liquor = liquorService.addLiquor(liquorDto);
-            if (liquor != null) {
-                return new ResponseEntity(liquor, HttpStatus.CREATED);
-            } else
-                return new ResponseEntity("An error occured while adding liquor", HttpStatus.BAD_REQUEST);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
 
-    @DeleteMapping("/deleteLiquor/{id}")
-    public ResponseEntity<?> deleteLiquor(@PathVariable long id) {
-        liquorService.deleteLiquorById(id);
-        return new ResponseEntity(HttpStatus.NO_CONTENT);
-    }
 
     @GetMapping("/allLiquors")
     public ResponseEntity<?> getAllLiquors() {
@@ -56,17 +40,5 @@ public class LiquorController {
         }
     }
 
-    @PutMapping("/updateLiquor/{id}")
-    public ResponseEntity<?> updateLiquor(@PathVariable long id, @RequestBody LiquorDto liquorDto) {
-        try {
-            Liquor updatedLiquor = liquorService.updateLiquor(id, liquorDto);
-            if (updatedLiquor != null) {
-                return new ResponseEntity<>(updatedLiquor, HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>("Liquor not found", HttpStatus.NOT_FOUND);
-            }
-        } catch (Exception e) {
-            return new ResponseEntity<>("An error occurred while updating liquor", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+
 }
